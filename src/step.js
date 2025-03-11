c005d5bbf84aa57a4db490a3d13efc14053c3c9e
Cornars <85560422+Cornars@users.noreply.github.com>
2025-03-11 13:59:07 +0800

    refactor: move scan to function - make each case in step a separate function - make scan function private

--- src/step.js
+++ src/step.js
@@ -18,19 +18,7 @@ export function step(sections, machineState) {
     switch (machineState.currentState.command) {
         // read right of input tape, then move head there. make sure to transition based on what was read
         case "SCAN":
-            console.log("machine:", machineState);
-
-            let scanRightValue =
-                machineState.singleLineInputText[
-                    machineState.currentHeadIndex + 1
-                ];
-            console.log("SCAN RIGHT: ", scanRightValue);
-            let nextStateName =
-                machineState.currentState.transitions[scanRightValue];
-            let nextStateObject = sections.logicSection[nextStateName];
-            machineState.currentState = nextStateObject;
-            console.log("TRANSITIONS: ", machineState.currentState.transitions);
-            machineState.currentHeadIndex++;
+            scan(sections, machineState);
         default:
             break;
     }
@@ -42,13 +30,17 @@ export function step(sections, machineState) {
 }
 /**
  *
- * @param {Object} inputTape
- * @param {*} currentIndex
- * @param {*} currentState
- * @param {*} transitionState
+ * @param {Object} macineState
  */
-export function scan(inputTape, currentIndex, currentState, transitions) {
-    head = inputTape[currentIndex];
-    const clone = structuredClone(currentState); // it just copy pastes the object, and doesn't pass a reference
-    console.log("clone");
+function scan(sections, machineState) {
+    console.log("======== START OF SCAN ========");
+    let scanRightValue =
+        machineState.singleLineInputText[machineState.currentHeadIndex + 1];
+    console.log("Value Scanned on Right: ", scanRightValue);
+    let nextStateName = machineState.currentState.transitions[scanRightValue];
+    let nextStateObject = sections.logicSection[nextStateName];
+    machineState.currentState = nextStateObject;
+    console.log("TRANSITION STATE: ", machineState.currentState);
+    machineState.currentHeadIndex++;
+    console.log("======== END OF SCAN ========");
 }
